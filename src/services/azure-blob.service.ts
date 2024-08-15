8463af0a74ba8b96a339ccbcb26744e8b356b1ae
Diego de Salles <diegosalles@areopagus.tech>
2024-08-15 17:23:46 -0300

    feat: azure blob storage testing

---
+++ src/services/azure-blob.service.ts
@@ -0,0 +1,43 @@
+import {
+  BlobServiceClient,
+  BlockBlobClient,
+  ContainerClient,
+} from '@azure/storage-blob';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AzureBlobService {
+  private blobServiceClient: BlobServiceClient;
+  private containerClient: ContainerClient;
+
+  constructor() {
+    const connectionString = process.env.AZURE_BLOB_STRING;
+    const containerName = 'teste';
+
+    this.blobServiceClient =
+      BlobServiceClient.fromConnectionString(connectionString);
+    this.containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
+  }
+
+  async uploadBlob(blobName: string, data: Buffer | Blob | string) {
+    const blockBlobClient: BlockBlobClient =
+      this.containerClient.getBlockBlobClient(blobName);
+
+    await blockBlobClient.upload(data, this.getDataLength(data));
+
+    return blockBlobClient.url;
+  }
+
+  private getDataLength(data: Buffer | Blob | string): number {
+    if (typeof data === 'string') {
+      return Buffer.byteLength(data);
+    } else if (data instanceof Buffer) {
+      return data.length;
+    } else if (data instanceof Blob) {
+      return data.size;
+    } else {
+      throw new Error('Unsupported data type');
+    }
+  }
+}
